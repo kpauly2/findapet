1fec1922fba46a54f6a0f5fd9eef2d540432c367
Kevin Pauly <kpauly2@ford.com>
2018-08-25 10:53:35 -0400

    Remove Espresso

--- app/src/androidTest/java/tech/pauly/findapet/utils/MockWebServerRequestPatterns.java
+++
@@ -1,7 +0,0 @@
-package tech.pauly.findapet.utils;
-
-import java.util.regex.Pattern;
-
-public class MockWebServerRequestPatterns {
-    public static final Pattern FETCH_ANIMALS = Pattern.compile("GET /pet\\.find");
-}
